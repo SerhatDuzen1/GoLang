6e2099e26a90b3a983484046c46cef5e10137bec
SerhatDuzen1 <serhat.duzen5959@gmail.com>
2022-04-13 21:13:18 +0300

    update

---
+++ 020_packages/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"math/rand"
+	"strings"
+	"github.com/fatih/color"
+	
+)
+
+func main() {
+
+	fmt.Println("Serhat")
+
+	fmt.Println("My favorite number is", rand.Intn(10))
+
+	//strings
+
+	fmt.Println(strings.Contains("Ali ata bak.","ta"))
+	fmt.Println(strings.Count("Üzüm üzüme baka baka kararır.","üzüm")
+fmt.Println(strings.HasPrefix("person_name","person")))
+fmt.Println(strings.HasSuffix("fifa99.exe","exe"))
+fmt.Println(strings.Index("Kara tren gecikir","n"))
+
+color.White("Bu bir Uyarıdır")
+
+
+}
